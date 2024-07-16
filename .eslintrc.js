5bba7b7d905e7256be5fb94d9241118a25d8af31
Aziyat Murzamidinov <15@kross.kr>
2024-07-16 17:56:30 +0900

    chore: configure prettier and add to cic

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
